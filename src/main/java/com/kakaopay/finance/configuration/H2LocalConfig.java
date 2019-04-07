73a4fca9e4c03ec365eb601a2afa436e0c155bf3
RongChoii <cyl6462@naver.com>
2019-04-08 04:43:18 +0900

    EntityManagerFactory 생성방법 변경

--- src/main/java/com/kakaopay/finance/configuration/H2LocalConfig.java
+++ src/main/java/com/kakaopay/finance/configuration/H2LocalConfig.java
@@ -1,34 +1,48 @@
 package com.kakaopay.finance.configuration;
 
 import lombok.Builder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class H2LocalConfig {
 
-    @Value("spring.datasource.url")
-    static String url;
+//    @Value("spring.datasource.url") //@@@@@@@@@@@
+    static String url = "jdbc:h2:file:C:/Users/rong/eclipse-workspace/financeDB;AUTO_SERVER=TRUE;";
+//    static String url = "jdbc:h2:tcp://localhost:8080/financeDB;AUTO_SERVER=TRUE";
+//    static String url = "jdbc:h2:mem:financeDB;DB_CLOSE_ON_EXIT=FALSE;";
 
-    @Value("spring.datasource.username")
-    static String username;
+//    @Value("spring.datasource.username")
+    static String username="kakao";
 
-    @Value("spring.datasource.password")
-    static String password;
+//    @Value("spring.datasource.password")
+    static String password="0000";
 
     public static EntityManagerFactory getEntityManagerFactory(){
-        return Persistence.createEntityManagerFactory(
-                EntityManagerUrlBuilder
-                        .builder()
-                        .url(url)
-                        .username(username)
-                        .password(password)
-                        .build()
-                        .getPersistenceUnitName()
-        );
+
+        System.out.println("@@@@ H2LocalConfig : getEntityMangerFactory : " + url + "/" + username + "/" + password);
+
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put("javax.persistence.jdbc.user", username);
+        properties.put("javax.persistence.jdbc.password", password);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+                url, properties);
+
+        return emf;
+
+//        return Persistence.createEntityManagerFactory(
+//                EntityManagerUrlBuilder
+//                        .builder()
+//                        .url(url)
+//                        .username(username)
+//                        .password(password)
+//                        .build()
+//                        .getPersistenceUnitName()
+//        );
     }
 
     @Builder
@@ -38,9 +52,9 @@ public class H2LocalConfig {
         private String password;
 
         public String getPersistenceUnitName(){
+            System.out.println("@@@@ H2LocalConfig : EntityManagerUrlNuilder");
+
             return url + ";user=" + username + ";password=" + password;
         }
-//    EntityManagerFactory emf = Persistence.createEntityManagerFactory(
-//            "objectdb://localhost/myDbFile.odb;user=admin;password=admin");
     }
 }

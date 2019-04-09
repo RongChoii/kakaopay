0dbf30c0f098c5314418395c6611bdf71d7a1937
RongChoii <cyl6462@naver.com>
2019-04-09 12:16:59 +0900

    SupplyDataRepository 추가

--- src/main/java/com/kakaopay/finance/jpa/SupplyDataRepository.java
+++ src/main/java/com/kakaopay/finance/jpa/SupplyDataRepository.java
@@ -2,7 +2,15 @@ package com.kakaopay.finance.jpa;
 
 import com.kakaopay.finance.model.file.SupplyData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface SupplyDataRepository extends JpaRepository<SupplyData, String> {
+
+    @Query(value = "select bank from supply_data group by bank", nativeQuery = true)
+    public List<Object[]> findBankList();
 
 }
